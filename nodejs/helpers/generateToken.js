4a56fdbd38e26ac5123ac17b7f5697f7aeb3e631
hubbusysyuhada <hubbusysyuhada@gmail.com>
2021-06-06 12:21:11 +0700

    folder re-arrangement

--- nodejs/helpers/generateToken.js
+++ nodejs/helpers/generateToken.js
@@ -1,16 +1,12 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 function token (payload) {
-    // payload.time = new Date()
-    // console.log(payload, '<<< payload');
-    // let generatedToken = jwt.sign(payload, process.env.JWT_KEY)
     const string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
     let temp = ''
     for (let i = 0; i < 50; i++) {
         temp += string[Math.floor(Math.random() * string.length)]
     }
     return temp
-    // return generatedToken
 }
 
 function decodeToken (token) {
